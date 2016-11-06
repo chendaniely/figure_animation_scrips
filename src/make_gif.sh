f35a1e6e3937258ce8b05759e52586af028166bb
Daniel Chen <chendaniely@users.noreply.github.com>
2016-11-06 17:29:14 -0500

    Update make_gif.sh

--- src/make_gif.sh
+++ src/make_gif.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/bash
 #
 # Uses imageMagick
+#
+# delay is in miliseconds
+# pass -1 for YES loop, 0 for NO loop
+# next parameters are files used to turn into gif
+# last parameter is the new filename
 
 convert -delay 100 -loop -1 plot_*.png out.gif
